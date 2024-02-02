ee4dc259d745358477b4975c3ec535bc5d0da265
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-02 15:30:21 -0700

    homework1

--- Homeworks/homework1.py
+++ Homeworks/homework1.py
@@ -2,12 +2,3 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-x = np.arange(1.920, 2.080, 0.001)
-y = x**9 - (18*x**8) + (144*x**7) - (672*x**6) + (2016*x**5) - (4032*x**4) + (5376*x**3) - (4608*x**2) + (2304*x) - 512
-plt.figure(1)
-plt.plot(x,y)
-
-plt.figure(2)
-l = (x-2)**9
-plt.plot(x,l)
-
