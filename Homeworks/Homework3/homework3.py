6733a2548412ab416919dbc0420063b18c3c8cd9
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-07 20:46:54 -0700

    commit

--- Homeworks/Homework3/homework3.py
+++ Homeworks/Homework3/homework3.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 def problem3(x):
-    y = np.e**2
-    return y-1
+    y = np.e**x
+    value = y - 1
+    return value
 
 x1 = 2.5
 ystar = problem3(x1)
 
-print ('ystar is', ystar)
+print ('ystar1 is', ystar)
