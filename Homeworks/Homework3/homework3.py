b18af0200d9e884a6809c22846bdb8b2986863d6
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-07 22:48:34 -0700

    homework3

--- Homeworks/Homework3/homework3.py
+++ Homeworks/Homework3/homework3.py
@@ -7,30 +7,31 @@ import matplotlib.pyplot as plt
 # creating a vector t at 0 to pi with increments of pi/30
 t = np.arange(0, np.pi, np.pi/30)
 # create a vector for y
-y = np.cos(t)
+y1 = np.cos(t)
 
 N = len(t)
 k = 1
 
 for k in range(len(t)):
-    S = t[k] * y[k]
+    S = t[k] * y1[k]
     k = k + 1
     print('the sum is', S)
-    break
 
 # Question b
 # Create variables
-theta = np.arange(0, np.pi, np.pi/2)
+theta = np.linspace(0, 2*np.pi, num = 10)
+print(theta)
 R = 1.2
 Sr = 0.1
-f =15
+f = 15
 p = 0
 
 x = R * (1 + Sr * np.sin(f * theta + p)) * np.cos(theta)
 y = R * (1 + Sr * np.sin(f * theta + p)) * np.sin(theta)
-
 #create plots
 plt.figure()
 plt.plot(theta, x, color = 'r')
 plt.plot(theta, y, color = 'blue')
-plt.show(block=False)
+plt.xlim(0, 2*np.pi)
+plt.ylim(-1.5, 1.5)
+plt.show()
