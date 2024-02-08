5671d890de150f21fcb31065af87df2d871903e5
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-07 22:19:33 -0700

    homework

--- Homeworks/Homework3/homework3.py
+++ Homeworks/Homework3/homework3.py
@@ -2,4 +2,14 @@ import numpy as np
 
 # creating a vector t at 0 to pi with increments of pi/30
 t = np.arange(0, np.pi, np.pi/30)
-print(t)
+# create a vector for y
+y = np.cos(t)
+
+N = np.range(t)
+k = 1
+
+for k in range(t):
+    S = t[k] * y[k]
+    k = k + 1
+    print('the sum is', S)
+
