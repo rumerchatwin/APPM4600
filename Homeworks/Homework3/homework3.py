a4029d3b280a37350e04b7051223d2956a37de34
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-07 23:03:15 -0700

    homework3

--- Homeworks/Homework3/homework3.py
+++ Homeworks/Homework3/homework3.py
@@ -18,20 +18,38 @@ for k in range(len(t)):
     print('the sum is', S)
 
 # Question b
-# Create variables
+# Create variables for first plot
 theta = np.linspace(0, 2*np.pi, num = 10)
-print(theta)
 R = 1.2
 Sr = 0.1
 f = 15
 p = 0
 
-x = R * (1 + Sr * np.sin(f * theta + p)) * np.cos(theta)
-y = R * (1 + Sr * np.sin(f * theta + p)) * np.sin(theta)
+x = R * np.cos(theta) * (1 + Sr * np.sin(f * theta + p)) 
+y = R * np.sin(theta) * (1 + Sr * np.sin(f * theta + p)) 
 #create plots
-plt.figure()
+plt.figure(1)
 plt.plot(theta, x, color = 'r')
 plt.plot(theta, y, color = 'blue')
 plt.xlim(0, 2*np.pi)
 plt.ylim(-1.5, 1.5)
 plt.show()
+
+
+#create varibles for plot 2
+Sr2 = 0.05
+p2 = np.random.uniform(0, 2)
+
+plt.figure(2)
+for i in range(5):
+    R2 = i
+    f = 2 + i
+    x2 = R2 * np.cos(theta) * (1 + Sr2 * np.sin(f * theta + p2)) 
+    y2 = R2 * np.sin(theta) * (1 + Sr2 * np.sin(f * theta + p2))
+    plt.plot(theta, x2, label = i)
+    plt.plot(theta, y2, label = i+1)
+    i = i + 1
+plt.show()
+
+
+
