3af1a66a1f32e9355ab717bc8fff7b1ffe85a02f
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-12 21:57:32 -0700

    prelab4

--- Labs/Lab4/lab4.py
+++ Labs/Lab4/lab4.py
@@ -20,6 +20,9 @@ def driver():
     print('f1(xstar):',f1(xstar))
     print('Error message reads:',ier)
     print('the iterations are', p_vector)
+    convergence = ooc(f1(xstar), p_vector)
+    print ('The convergence is', convergence)
+
 #test f2 '''
     x0 = 0.0
     [xstar,ier,p_vector] = fixedpt(f2,x0,tol,Nmax)
@@ -27,6 +30,8 @@ def driver():
     print('f2(xstar):',f2(xstar))
     print('Error message reads:',ier)
     print('the iterations are', p_vector)
+    convergence = ooc(f2(xstar), p_vector)
+    print ('The convergence is', convergence)
 
 
 
@@ -36,6 +41,7 @@ def fixedpt(f,x0,tol,Nmax):
     ''' x0 = initial guess''' 
     ''' Nmax = max number of iterations'''
     ''' tol = stopping tolerance'''
+
     p_vector = np.zeros((Nmax, 1))
     count = 0
     while (count < Nmax):
@@ -45,15 +51,26 @@ def fixedpt(f,x0,tol,Nmax):
         if (abs(x1-x0) <tol):
             xstar = x1
             ier = 0
-            return [xstar,ier,]
+            return [xstar,ier, p_vector]
         x0 = x1
 
     xstar = x1
     ier = 1
     return [xstar, ier,p_vector]
 
+def ooc(p, p_vector):
+    n = 0
+    convergence = 0
+    while n in range(2):
+        limit = abs(p_vector[n+1] - p) / abs(p_vector[n] - p)**n
 
-
-
+        if (n == 1):
+            if(limit < 1):
+                return[n]
+        elif(n == 2):
+            convergence = 2
+            return[n]
+    
+    return[n]
 
 driver()
