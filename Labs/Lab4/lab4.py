2e9eed29389b3cf759b73389acd33c3e965095dc
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-12 21:16:44 -0700

    commit

--- Labs/Lab4/lab4.py
+++ Labs/Lab4/lab4.py
@@ -13,16 +13,17 @@ def driver():
     Nmax = 100
     tol = 1e-6
 
+
 # test f1 '''
     x0 = 0.0
-    [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+    [xstar,ier,p_vector] = fixedpt(f1,x0,tol,Nmax)
     print('the approximate fixed point is:',xstar)
     print('f1(xstar):',f1(xstar))
     print('Error message reads:',ier)
     
 #test f2 '''
     x0 = 0.0
-    [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+    [xstar,ier,p_vector] = fixedpt(f2,x0,tol,Nmax)
     print('the approximate fixed point is:',xstar)
     print('f2(xstar):',f2(xstar))
     print('Error message reads:',ier)
@@ -35,19 +36,20 @@ def fixedpt(f,x0,tol,Nmax):
     ''' x0 = initial guess''' 
     ''' Nmax = max number of iterations'''
     ''' tol = stopping tolerance'''
-
+    p_vector = np.zeros((Nmax, 1))
     count = 0
-    while (count <Nmax):
-        count = count +1
+    while (count < Nmax):
         x1 = f(x0)
+        p_vector[count]= x1
+        count = count + 1
         if (abs(x1-x0) <tol):
             xstar = x1
             ier = 0
-            return [xstar,ier]
+            return [xstar,ier,p_vector]
         x0 = x1
 
     xstar = x1
     ier = 1
-    return [xstar, ier]
+    return [xstar, ier,p_vector]
     
 driver()
