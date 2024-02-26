fe0e78cb82ebce73627abb7ee361c8bbe25f1861
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-26 13:09:45 -0700

    prelab6

--- Labs/Lab6/prelab6.py
+++ Labs/Lab6/prelab6.py
@@ -1,20 +1,6 @@
 import numpy as np
 
 #prelab 6
-def driver():
-    f = lambda x: np.cos(x)
-    h = - np.arange(0, 10) * 2 
-    
-    x0 = np.pi/2
-
-    [f_vector] = foward_difference(f, x0, h)
-    print('The vector is', f_vector)
-
-    [c_vector] = centered_difference(f, x0, h)
-    print('The vector is', c_vector)
-
-
-
 #define the foward difference
 def foward_difference(f, x, h):
     count = 1
@@ -23,7 +9,8 @@ def foward_difference(f, x, h):
         f_prime = (f(x + h[count]) - f(x)) / (h[count])
         vector.append(f_prime)
         count =+ 1
-
+    
+    last = f_prime
     return [vector]
 
 def centered_difference(f, x, h):
@@ -35,8 +22,19 @@ def centered_difference(f, x, h):
         vector.append(f_prime)
         count =+ 1
 
-    return[vector]
+    last = f_prime
+    return[vector, last]
         
 
+f = lambda x: np.cos(x)
+h = - np.arange(0, 10) * 2 
+    
+x0 = np.pi/2
 
-driver()
+[f_vector, final] = foward_difference(f, x0, h)
+print('The vector is', f_vector)
+print('The derivative is', final)
+
+[c_vector, c_final] = centered_difference(f, x0, h)
+print('The vector is', c_vector)
+print('The derivative is', c_final)
