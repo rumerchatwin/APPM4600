ded0408fbe493267ab5613e725bdf4007079aad7
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-26 12:04:49 -0700

    prelab6

--- Labs/Lab6/prelab6.py
+++ Labs/Lab6/prelab6.py
@@ -1,14 +1,57 @@
 import numpy as np
 
-f = lambda s: np.cos(s)
-h = np.arange(0,10)
-x = np.pi/2
+#prelab 6
+def driver():
+    f = lambda x: np.cos(x)
+    h = - np.arange(0, 10)
+    nmax = 100
+    tol = 10e10
+    
+    x0 = np.pi/2
 
-foward = (f(x + h) - f(x)) / h
-centered = (f(x + h) - f(x-h)) / 2 * h
+    [f_der, f_ier, f_vector] = foward_difference(f, x0, h, nmax, tol)
+    print('The foward difference is', f_der)
+    print('The vector is', f_vector)
+    print('The error is', f_ier)
 
-derivative = -np.sin(x)
+    [c_der, c_ier, c_vector] = centered_difference(f, x0, h, nmax, tol)
+    print('The centered difference is', c_der)
+    print('The vector is', c_vector)
+    print('The error is', c_ier)
 
-print('The Foward Difference apporximation is', foward)
-print('The Centered Difference approximation is', centered)
-print('The deriavtive is', derivative)
+
+
+#define the foward difference
+def foward_difference(f, x, h, nmax, tol):
+    count = 0
+    vector = []
+    while count < nmax:
+        f_prime = (f(x + h[count]) - f(x)) / h[count]
+        vector.append(f_prime)
+        count =+ 1
+        if (f_prime < tol):
+            derivative = f_prime
+            ier = 0
+            return [derivative, ier, vector]
+    
+    ier = 1
+    return [f_prime, ier, vector]
+
+def centered_difference(f, x, h, nmax, tol):
+    count = 0
+    vector = []
+    
+    while count < nmax:
+        f_prime = (f(x + h[count]) - f(x - h[count])) / 2 * h[count]
+        vector.append(f_prime)
+        count =+ 1
+        if (f_prime < tol):
+            derivative = f_prime
+            ier = 0
+            return[derivative, ier, vector]
+        
+    ier = 1
+    return[ derivative, ier, vector]
+
+
+
