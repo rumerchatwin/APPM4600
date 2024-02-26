b01bfedc016aa3d63188c11fc93f26fa6538222e
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-26 13:07:31 -0700

    prelab6

--- Labs/Lab6/prelab6.py
+++ Labs/Lab6/prelab6.py
@@ -7,39 +7,36 @@ def driver():
     
     x0 = np.pi/2
 
-    [f_der, f_vector] = foward_difference(f, x0, h)
-    print('The foward difference is', f_der)
+    [f_vector] = foward_difference(f, x0, h)
     print('The vector is', f_vector)
 
-    [c_der, c_vector] = centered_difference(f, x0, h)
-    print('The centered difference is', c_der)
+    [c_vector] = centered_difference(f, x0, h)
     print('The vector is', c_vector)
 
 
 
 #define the foward difference
 def foward_difference(f, x, h):
-    count = 0
+    count = 1
     vector = []
-    while count <= len(h):
-        f_prime = (f(x + h[count]) - f(x)) / h[count]
+    while (count <= len(h)):
+        f_prime = (f(x + h[count]) - f(x)) / (h[count])
         vector.append(f_prime)
         count =+ 1
 
-    return [f_prime, vector]
+    return [vector]
 
 def centered_difference(f, x, h):
-    count = 0
+    count = 1
     vector = []
     
-    while count <= len(h):
+    while (count <= len(h) ):
         f_prime = (f(x + h[count]) - f(x - h[count])) / 2 * h[count]
         vector.append(f_prime)
         count =+ 1
 
-    return[f_prime, vector]
+    return[vector]
         
 
 
 driver()
-
