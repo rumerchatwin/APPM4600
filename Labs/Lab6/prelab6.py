4844782cd94769576116ed9627a70034f030aeae
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-26 22:06:13 -0700

    prelab6

--- Labs/Lab6/prelab6.py
+++ Labs/Lab6/prelab6.py
@@ -3,18 +3,18 @@ import numpy as np
 def driver():
     f = lambda x: np.cos(x)
     x0 = np.pi/2
-    h = (0.01 * 2) ** (-np.arange(0,10))
+    h = 0.01 / 2 ** np.arange(0,10)
 
-    [foward_derivative] = foward(f, h, x0)
-    [centered_derivative] = centered(f,h,x0)
-    print('The foward difference appis', foward_derivative)
+    foward_derivative = foward(f, h, x0)
+    centered_derivative = centered(f,h,x0)
+    print('The foward difference approximation', foward_derivative)
     print('The centered difference approximation is', centered_derivative)
 
 def foward(f, h, x0):
     derivative = (f(x0 + h) - f(x0)) / h
     return[derivative]
 def centered(f, h, x0):
-    derivative = (f(x0 + h) - f(x0 - h))/ 2 * h
+    derivative = (f(x0 + h) - f(x0 - h))/ (2 * h)
     return[derivative]
 
 driver()
