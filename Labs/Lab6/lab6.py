001521313ea58b9a5145c98c1add5bb9f244a9f8
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-02-27 09:58:18 -0700

    lab6

--- Labs/Lab6/lab6.py
+++ Labs/Lab6/lab6.py
@@ -7,18 +7,104 @@ from numpy.linalg import norm
 # define routines
 def evalF(x): 
 
-    F = np.zeros(3)
+    #F = np.zeros(3)
     
-    F[0] = 3*x[0]-math.cos(x[1]*x[2])-1/2
-    F[1] = x[0]-81*(x[1]+0.1)**2+math.sin(x[2])+1.06
-    F[2] = np.exp(-x[0]*x[1])+20*x[2]+(10*math.pi-3)/3
+    #F[0] = 3*x[0]-math.cos(x[1]*x[2])-1/2
+    #F[1] = x[0]-81*(x[1]+0.1)**2+math.sin(x[2])+1.06
+    #F[2] = np.exp(-x[0]*x[1])+20*x[2]+(10*math.pi-3)/3
+
+    F = np.zeros(2)
+    F[0] = 4 * x[0]**2 + x[1]**2 -4
+    F[1] = x[0] + x[1] - math.sin(x[0] - x[1])
     return [F]
     
 def evalJ(x): 
-    J = np.array([[3.0, x[2]*math.sin(x[1]*x[2]), x[1]*math.sin(x[1]*x[2])], 
-        [2.*x[0], -162.*(x[1]+0.1), math.cos(x[2])], 
-        [-x[1]*np.exp(-x[0]*x[1]), -x[0]*np.exp(-x[0]*x[1]), 20]])
+    #J = np.array([[3.0, x[2]*math.sin(x[1]*x[2]), x[1]*math.sin(x[1]*x[2])], 
+        #[2.*x[0], -162.*(x[1]+0.1), math.cos(x[2])], 
+        #[-x[1]*np.exp(-x[0]*x[1]), -x[0]*np.exp(-x[0]*x[1]), 20]])
+    J = np.array([[8*x[0] , 2*x[1]],
+                 [1 - math.cos(x[0] - x[1]) , x[1] + math.cos(x[0] - x[1])]])
+
     return [J]
+
+def driver():
+    xo = np.array([[1],
+                  [0]])
+    tol = 10E-10
+    Nmax = 100
+    t = time.time()
+
+    for j in range(20):
+        [xstar,ier,its] =  Newton(xo,tol,Nmax)
+        elapsed = time.time()-t
+        print(xstar)
+        print('Newton: the error message reads:',ier)
+        print('Newton: took this many seconds:',elapsed/20)
+        print('Netwon: number of iterations is:',its)
+    
+    for j in range(20):
+        [xstar,ier,its] =  LazyNewton(xo,tol,Nmax)
+        elapsed = time.time()-t
+        print(xstar)
+        print('Lazy Newton: the error message reads:',ier)
+        print('Lazy Newton: took this many seconds:',elapsed/20)
+        print('Lazy Newton: number of iterations is:',its)
+
+    for j in range(20):
+        [xstar,ier,its] =  SlackerNewton(xo,tol,Nmax)
+        elapsed = time.time()-t
+        print(xstar)
+        print('Lazy Newton: the error message reads:',ier)
+        print('Lazy Newton: took this many seconds:',elapsed/20)
+        print('Lazy Newton: number of iterations is:',its)
+
+
+def Newton(x0,tol,Nmax):
+
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+
+    for its in range(Nmax):
+       J = evalJ(x0)
+       Jinv = inv(J)
+       F = evalF(x0)
+       
+       x1 = x0 - Jinv.dot(F)
+       
+       if (norm(x1-x0) < tol):
+           xstar = x1
+           ier =0
+           return[xstar, ier, its]
+           
+       x0 = x1
+    
+    xstar = x1
+    ier = 1
+    return[xstar,ier,its]
+           
+def LazyNewton(x0,tol,Nmax):
+
+    ''' Lazy Newton = use only the inverse of the Jacobian for initial guess'''
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+
+    J = evalJ(x0)
+    Jinv = inv(J)
+    for its in range(Nmax):
+
+       F = evalF(x0)
+       x1 = x0 - Jinv.dot(F)
+       
+       if (norm(x1-x0) < tol):
+           xstar = x1
+           ier =0
+           return[xstar, ier,its]
+           
+       x0 = x1
+    
+    xstar = x1
+    ier = 1
+    return[xstar,ier,its]   
 
 # Lab 6
 def SlackerNewton(x0,tol,Nmax):
@@ -34,7 +120,7 @@ def SlackerNewton(x0,tol,Nmax):
        F = evalF(x0)
        x1 = x0 - Jinv.dot(F)
 
-       if(abs(x1) > abs(x0)):
+       if(abs(norm(evalF(x1))) > abs(norm(evalF(x0)))):
            Jinv = inv(Jinv)
            new_inverse =+ 1
 
@@ -48,3 +134,6 @@ def SlackerNewton(x0,tol,Nmax):
     xstar = x1
     ier = 1
     return[xstar,ier,its,new_inverse]   
+
+
+driver()
