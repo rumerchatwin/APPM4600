0267d81273001fbe0096ad8072a8ac8ae9d62955
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-03-04 08:48:05 -0700

    prelab7

--- Labs/Lab7/interp.py
+++ Labs/Lab7/interp.py
@@ -2,16 +2,13 @@ import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
 
+''' Main Driver for code (From Example)'''
+
 def driver():
-
-
     f = lambda x: np.exp(x)
-
     N = 3
-    ''' interval'''
     a = 0
     b = 1
-   
    
     ''' create equispaced interpolation nodes'''
     xint = np.linspace(a,b,N+1)
@@ -37,22 +34,14 @@ def driver():
     for kk in range(Neval+1):
        yeval_l[kk] = eval_lagrange(xeval[kk],xint,yint,N)
        yeval_dd[kk] = evalDDpoly(xeval[kk],xint,y,N)
-          
-
-    
-
-
     ''' create vector with exact values'''
     fex = f(xeval)
-       
-
-    plt.figure()    
+    plt.figure(1)    
     plt.plot(xeval,fex,'ro-')
     plt.plot(xeval,yeval_l,'bs--') 
     plt.plot(xeval,yeval_dd,'c.--')
     plt.legend()
-
-    plt.figure() 
+    plt.figure(2) 
     err_l = abs(yeval_l-fex)
     err_dd = abs(yeval_dd-fex)
     plt.semilogy(xeval,err_l,'ro--',label='lagrange')
@@ -60,6 +49,7 @@ def driver():
     plt.legend()
     plt.show()
 
+''' The Lagrange Code '''
 def eval_lagrange(xeval,xint,yint,N):
 
     lj = np.ones(N+1)
@@ -76,22 +66,18 @@ def eval_lagrange(xeval,xint,yint,N):
   
     return(yeval)
   
-    
-
-
 ''' create divided difference matrix'''
 def dividedDiffTable(x, y, n):
  
     for i in range(1, n):
         for j in range(n - i):
-            y[j][i] = ((y[j][i - 1] - y[j + 1][i - 1]) /
-                                     (x[j] - x[i + j]));
-    return y;
-    
+            y[j][i] = ((y[j][i - 1] - y[j + 1][i - 1]) / (x[j] - x[i + j]))
+    return y
+
+''' Evaluating the polynomial terms and DD polynomial '''
 def evalDDpoly(xval, xint,y,N):
     ''' evaluate the polynomial terms'''
     ptmp = np.zeros(N+1)
-    
     ptmp[0] = 1.
     for j in range(N):
       ptmp[j+1] = ptmp[j]*(xval-xint[j])
@@ -103,6 +89,5 @@ def evalDDpoly(xval, xint,y,N):
 
     return yeval
 
-       
-
+''' Code that runs '''
 driver()        
