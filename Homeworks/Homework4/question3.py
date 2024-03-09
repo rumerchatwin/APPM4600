39ee6d6d2e90205ddc6063cc462023c29dd239b7
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-03-08 19:57:12 -0700

    homework4

--- Homeworks/Homework4/question3.py
+++ Homeworks/Homework4/question3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 import numpy.linalg as la
 from numpy.linalg import inv 
@@ -8,22 +9,28 @@ from numpy.linalg import norm
 ''' The inital guess is x = 0, y = 0, and  z = 0'''
 def driver():
     tol = 10E-6
-    Nmax = 100
+    Nmax = 10
     x0 = 0; y0 = 0; z0= 0
+    t = time.time()
 
-    xfinal = Newton(x0, y0, z0, tol, Nmax)
-    print(' The newton method has a vector of', xfinal)
+    for j in range(20):
+        xfinal =  Newton(x0, y0, z0, tol,Nmax)
+        elapsed = time.time()-t
+        print(xfinal)
+        print('Newton: took this many seconds:',elapsed/20)
+
+    
 
 ''' Finding the matrix F and Jacobian matrix '''
 def evalF(x, y, z):
-    F = np.zeros(3)
-    F[0] = x + np.cos(x * y * z) -1
-    F[1] = (1 - x)**(1/4) + y + 0.05*z**2 - 0.12*z -1
-    F[2] = -x**2 - 0.1*y**2 +z - 1
+    F = np.array([[x + np.cos(x * y * z) -1],
+                  [(1 - x)**(1/4) + y + 0.05*z**2 - 0.12*z -1],
+                  [-x**2 - 0.1*y**2 +z - 1]])
+    
     return [F]
 
 def evalJ(x, y, z):
-    J = np.array([ [1 - y*z*np.sin(x*y*z), -x*z*np.sin(x*y*z), -x*y*np.sin(x*y*z)],
+    J = np.array([ [(1 - y*z*np.sin(x*y*z)), -x*z*np.sin(x*y*z), -x*y*np.sin(x*y*z)],
                   [(1/4)*(1-x)**(-3/4), 1, 0.1*z - 0.15],
                   [-2*x, -0.2*y + 0.01, 1] ])
     return[J]
@@ -32,22 +39,20 @@ def evalJ(x, y, z):
 def Newton(x0, y0, z0, tol, Nmax):
 
     for i in range(Nmax):
-        x0 = np.array([[x0],
+        xvector = np.array([[x0],
                        [y0],
                        [z0]])
         F = evalF(x0, y0, z0)
         J = evalJ(x0, y0, z0)
         Jinv = inv(J)
 
-        x1 = x0 - np.dot(Jinv, F)
+        x1 = xvector - np.dot(Jinv, F)
 
         if (norm(x1 - x0) < tol):
             xfinal = x1
             return(xfinal)
         
-        x0 = x1[0]
-        y0 = x1[1]
-        z0 = x1[2]
+        [x0, y0, z0] = x1
 
     return(xfinal)
     
--- Labs/Lab6/lab6.py
+++ Labs/Lab6/lab6.py
@@ -19,9 +19,9 @@ def evalF(x):
     return [F]
     
 def evalJ(x): 
-    #J = np.array([[3.0, x[2]*math.sin(x[1]*x[2]), x[1]*math.sin(x[1]*x[2])], 
-        #[2.*x[0], -162.*(x[1]+0.1), math.cos(x[2])], 
-        #[-x[1]*np.exp(-x[0]*x[1]), -x[0]*np.exp(-x[0]*x[1]), 20]])
+    J = np.array([[3.0, x[2]*math.sin(x[1]*x[2]), x[1]*math.sin(x[1]*x[2])], 
+        [2.*x[0], -162.*(x[1]+0.1), math.cos(x[2])], 
+        [-x[1]*np.exp(-x[0]*x[1]), -x[0]*np.exp(-x[0]*x[1]), 20]])
     J = np.array([[8*x[0] , 2*x[1]],
                  [1 - math.cos(x[0] - x[1]) , x[1] + math.cos(x[0] - x[1])]])
 
