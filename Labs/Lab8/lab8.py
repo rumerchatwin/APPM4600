bd3728c1dae7375e7099dbc595fe93e2220c8b12
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-03-12 09:36:12 -0600

    lab8

--- Labs/Lab8/lab8.py
+++ Labs/Lab8/lab8.py
@@ -8,7 +8,7 @@ def line(x0, x1, f, alpha):
     point2 = f(x1)
 
     slope = (f(x1)-f(x0))/(x1-x0)
-    y_intercept = (slope * x1) - x0
+    y_intercept = (slope * -x1) + point2
     linear_function = lambda x: slope*x + y_intercept
 
     alpha_function = linear_function(alpha)
@@ -24,23 +24,50 @@ def driver():
     Nint = 10
     yeval = eval_lin_spline(xeval,Neval,a,b,f,Nint)
     fex = f(xeval)
-    plt.figure()
-    plt.plot(xeval,fex,'ro-')
-    plt.plot(xeval,yeval,'bs-')
+
+    plt.figure(1)
+    plt.plot(xeval,fex,'ro-', label = 'fex')
+    plt.plot(xeval,yeval,'bs-', label = 'yeval')
     plt.legend()
-    plt.show
+    plt.show()
+
     err = abs(yeval-fex)
-    plt.figure()
+    plt.figure(2)
     plt.plot(xeval,err,'ro-')
-    plt.show
+    plt.show()
+
+def excercise32():
+    f = lambda x: 1/(1 + (10*x)**2)
+    a = 1
+    b = -1
+    Neval = 100
+    xeval = np.linspace(a,b,Neval)
+    Nint = 10
+    yeval = eval_lin_spline(xeval, Neval, a , b, f, Nint)
+    fex = f(xeval)
+
+    plt.figure(1)
+    plt.plot(xeval,fex,'ro-', label = 'fex')
+    plt.plot(xeval,yeval,'bs-', label = 'yeval')
+    plt.legend()
+    plt.show()
+
+    err = abs(yeval-fex)
+    plt.figure(2)
+    plt.plot(xeval,err,'ro-')
+    plt.show()
+    
+
+
 def eval_lin_spline(xeval,Neval,a,b,f,Nint):
+
     xint = np.linspace(a,b,Nint+1)
     yeval = np.zeros(Neval)
     for jint in range(Nint):
-        atmp = xint[j]
-        btmp= xint[j+1]
+        atmp = xint[jint]
+        btmp = xint[jint+1]
         # find indices of values of xeval in the interval
-        ind= np.where((xeval >= atmp) & (xeval <= btmp))
+        ind = np.where((xeval >= atmp) & (xeval <= btmp))
         xloc = xeval[ind]
         n = len(xloc)
         fa = f(atmp)
@@ -48,8 +75,12 @@ def eval_lin_spline(xeval,Neval,a,b,f,Nint):
         yloc = np.zeros(len(xloc))
         for kk in range(n):
             #use your line evaluator to evaluate the spline at each location
-            yloc[kk] = #Call your line evaluator with points (atmp,fa) and (btmp,fb)
+            yloc[kk] = line(atmp, btmp, f, kk)
+            print(yloc[kk])
             # Copy yloc into the final vector
             yeval[ind] = yloc
     return yeval
-driver()
+
+
+#driver()
+#excercise32()
