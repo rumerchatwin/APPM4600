560b56b70f4afedd4f9ea51cd922f8a367e1cede
rumerchatwin <158119029+rumerchatwin@users.noreply.github.com>
2024-04-01 13:44:12 -0600

    prelab10

---
+++ Labs/Lab10/lab10.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.linalg as la
+import math
+from scipy.integrate import quad
+from scipy.special import legendre
+
+
+def eval_legendre(n, x):
+    p = np.zeros(n + 1)
+    p[0] = 1
+    p[1] = x
+    i = 1
+    for i in range(n):
+        p[i+1] = (1/(i+1)) * ( ( (2*i+1)*(x*p[i]) - i*p[i-1]) )
+
+    return(p)
+
+def prelab10():
+    n = 2
+    x = 1
+    p = eval_legendre(n, x)
+
+
+prelab10()
+# Code for later
+'''def driver():
+    # function you want to approximate
+    f = lambda x: math.exp(x)
+    # Interval of interest
+    a = -1
+    b = 1
+    # weight function
+    w = lambda x: 1.
+    # order of approximation
+    n = 2
+    # Number of points you want to sample in [a,b]
+    N = 1000
+    xeval = np.linspace(a,b,N+1)
+    pval = np.zeros(N+1)
+    for kk in range(N+1):
+        pval[kk] = eval_legendre_expansion(f,a,b,w,n,xeval[kk])
+
+    fex = np.zeros(N+1)
+    for kk in range(N+1):
+        fex[kk] = f(xeval[kk])
+    plt.figure()
+    plt.plot(xeval,fex,'ro-', label= 'f(x)')
+    plt.plot(xeval,pval,'bs--',label= 'Expansion')
+    plt.legend()
+    plt.show()
+    err_l = abs(pval-fex)
+
+    plt.semilogy(xeval,err_l,'ro--',label='error')
+    plt.legend()
+    plt.show()
+
+def eval_legendre_expansion(f,a,b,w,n,x):
+    # This subroutine evaluates the Legendre expansion
+    # Evaluate all the Legendre polynomials at x that are needed
+    # by calling your code from prelab
+    p = ...
+    # initialize the sum to 0
+    pval = 0.0
+    for j in range(0,n+1):
+    # make a function handle for evaluating phi_j(x)
+    phi_j = lambda x: ...
+    # make a function handle for evaluating phi_j^2(x)*w(x)
+    phi_j_sq = lambda x: ...
+    # use the quad function from scipy to evaluate normalizations
+    norm_fac,err = ...
+    # make a function handle for phi_j(x)*f(x)*w(x)/norm_fac
+    func_j = lambda x: ...
+    # use the quad function from scipy to evaluate coeffs
+    aj,err = ...
+    # accumulate into pval
+    pval = pval+aj*p[j]
+    return pval
+    if __name__ == '__main__':
+    # run the drivers only if this is called from the command line'''
+
